85712adfcbcd7cbc423b5845782b95b90db9827d
RomanticBox <123309107+RomanticBox@users.noreply.github.com>
2025-06-05 04:21:18 +0000

    Deploying to gh-pages from @ RomanticBox/RomanticBox.github.io@7443b3b3ba60c8dc500bb85d00684310b137fb0d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%65%65%70%73%77%6F%6F%6C@%79%6F%6E%73%65%69.%61%63.%6B%72", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/RomanticBox", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -509,6 +516,27 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/", "_blank");
+        },
+      },{
+        id: 'social-kaggle',
+        title: 'Kaggle',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.kaggle.com/romanbox", "_blank");
+        },
+      },{
+        id: 'social-leetcode',
+        title: 'LeetCode',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://leetcode.com/u/romanticBox/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -525,11 +553,18 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/iseonghyeo95180", "_blank");
+        },
+      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.romanticbox.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
