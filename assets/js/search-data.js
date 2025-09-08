6c96037babbfb04a37c2da0f895588eed8a1a149
RomanticBox <123309107+RomanticBox@users.noreply.github.com>
2025-09-08 07:54:31 +0000

    Deploying to gh-pages from @ RomanticBox/RomanticBox.github.io@bff2caf578986f03b27c051e28cb668e6f2fd67e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,9 +79,9 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-사랑하고-사랑-받는-모든-것을-위한-책방-네이버-블로그",
+            },{id: "post-사랑하고-사랑-받는-모든-것을-위한-책장-네이버-블로그",
         
-          title: '사랑하고 사랑 받는 모든 것을 위한 책방 : 네이버 블로그 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: '사랑하고 사랑 받는 모든 것을 위한 책장 : 네이버 블로그 <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "",
         section: "Posts",
